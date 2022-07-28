b69a1e97107a72da31ce5febde32ecfb16ad414e
Chandan Pratihast <chandanpratihast@gmail.com>
2022-07-28 12:34:41 +0900

    result is saved as object in file under data folder

--- MonitorFolder/src/sizecheck/CheckSize.java
+++ MonitorFolder/src/sizecheck/CheckSize.java
@@ -1,6 +1,18 @@
 package sizecheck;
 
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
 import java.util.HashMap;
 
 import org.apache.commons.io.FileUtils;
@@ -20,22 +32,27 @@ public class CheckSize {
 		
 		recurseNLevelDirSize(dir,0);
 //		System.out.println(map.toString());
-		
-		
-		
+		String d = "./data";
+		String f = new SimpleDateFormat("ddMMyyyy").format(new Date())+Instant.now().getNano();
+		try {			save(d,f);			} catch (IOException e) 			{	System.out.println("Error in saving file"+e.getMessage());		}
+		try {			map = load(d,f);	} catch (IOException e) 			{	System.out.println("Error in loading file"+e.getMessage());		}
+											  catch (ClassNotFoundException e) 	{	System.out.println("Error in casting object"+e.getMessage());	}
+//		System.out.println(map.toString());
 		
 	}
 	
 	public static void recurseNLevelDirSize(File folder, int curlevel) {
 		
 		if(curlevel > level ) { return; }
-		System.out.println("Checked..."+folder.getAbsolutePath());
+		
 		boolean noSubDir = true;
 		File[] all = folder.listFiles();
 		for (File f : all) {
 			if(!f.canRead()) {System.out.println("Can't read it:"+ f.getAbsolutePath()); continue;} 
 			boolean isDir = f.isDirectory();
+			
 			if (isDir) {
+				System.out.println("Checking..."+f.getAbsolutePath());
 				curlevel ++;
 				noSubDir = false;
 				long sz = FileUtils.sizeOfDirectory(f) / (1024 * 1024); // original size is bytes, so converted to MB.
@@ -48,4 +65,31 @@ public class CheckSize {
 			return;
 	}
 
+	public static void save(String dir, String fn) throws IOException {
+		Instant st = Instant.now();
+		File d = new File(dir);
+		if(!d.exists()) d.mkdir();
+		File f = new File(d+"/"+fn);
+		try(FileOutputStream fos = new FileOutputStream(f); ObjectOutputStream oos = new ObjectOutputStream(fos)){
+			oos.writeObject(map);
+			oos.flush();
+		}
+		Instant et = Instant.now();
+		System.out.println("Completed save in [ "+Duration.between(st, et).toMillis()+" ] ms");
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static HashMap<String,Long> load(String d, String f) throws IOException, ClassNotFoundException {
+		Instant st = Instant.now();
+		HashMap<String,Long> r = null;
+		try(
+		FileInputStream fis = new FileInputStream( d+"/"+f);
+		ObjectInputStream ois = new ObjectInputStream(fis) ) {
+			r = (HashMap<String,Long>)ois.readObject();
+		}
+		
+		Instant et = Instant.now();
+		System.out.println("Completed save in [ "+Duration.between(st, et).toMillis()+" ] ms");
+		return r;
+	}
 }
