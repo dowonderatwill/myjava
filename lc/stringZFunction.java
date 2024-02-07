dba65605e2e4d9f1065c5cbfcab15a443b043e02
dowonderatwill <chandanpratihast@gmail.com>
2024-02-07 22:40:25 +0900

    Create stringZFunction.java
    The Z function of String s. Z function is an array Z such that Z[i] = x, where x is the longest prefix of s also the prefix of Z[i...]

---
+++ lc/stringZFunction.java
@@ -0,0 +1,25 @@
+
+ public int[] getZ(char[] s){
+        int n = s.length;
+        int z[]= new int[n];
+        if(n==0) return z;
+        z[0] = n;
+        int l=0, r = 0; 
+        for( int i=1; i<n; i++){
+          if(i > r){
+            l = r = i;
+					  while(r < n && s[r-l] == s[r])r++;
+					  z[i] = r-l; r--;
+          }else{
+                if ( z[i-l] < r - i + 1)
+                    z[i] = z[i - l];
+                else{
+                    l = i;
+                    while (r<n && s[r-l] == s[r]) r++;
+                    z[i]=r-l;  r--;
+                }
+             }
+        }
+        return z;
+
+}// end of getZ
