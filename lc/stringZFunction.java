2bc16ab350f7ec8eecb30a2108ec7ed884063bc5
dowonderatwill <chandanpratihast@gmail.com>
2024-02-11 18:10:25 +0900

    format corrected

--- lc/stringZFunction.java
+++ lc/stringZFunction.java
@@ -8,8 +8,8 @@
         for( int i=1; i<n; i++){
           if(i > r){
             l = r = i;
-					  while(r < n && s[r-l] == s[r])r++;
-					  z[i] = r-l; r--;
+		while(r < n && s[r-l] == s[r])r++;
+			z[i] = r-l; r--;
           }else{
                 if ( z[i-l] < r - i + 1)
                     z[i] = z[i - l];
